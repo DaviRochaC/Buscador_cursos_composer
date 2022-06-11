1c3af698398658a21e3ee72106c5c3848d73986c
davirocha <davirocha2002.dr@gmail.com>
2022-06-11 10:58:14 -0300

    firs commit

---
+++ buscar-cursos.php
@@ -0,0 +1,17 @@
+<?php
+
+use Davi\BuscadorCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+require __DIR__.'/vendor/autoload.php';
+$client = new Client(['base_uri' => 'https://www.alura.com.br']);
+$crawler =  new Crawler();
+
+
+$buscador = new Buscador($client,$crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso){
+    echo $curso .PHP_EOL;
+}
---
+++ src/Buscador.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Davi\BuscadorCursos;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador
+{
+    private ClientInterface $httpClient;
+    private Crawler $crawler;
+
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
+
+    public function buscar(string $url): array
+    {
+        $response = $this->httpClient->request('GET', $url);
+
+        $html = $response->getBody();
+        $this->crawler->addHtmlContent($html);
+
+        $elementos = $this->crawler->filter('span.card-curso__nome');
+        $cursos = [];
+
+        foreach ($elementos as $elemento) {
+            $cursos[] = $elemento->textContent;
+        }
+         return $cursos;
+    }
+}
